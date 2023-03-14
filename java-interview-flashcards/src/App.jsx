fb705324986699fdc2b7db3e7b8ed0c33b586caa
Kavan Adeshara <73504280+kavanAdeshara@users.noreply.github.com>
2023-03-13 20:57:39 -0400

    added functionality to go back and forth and enter input for some questions

--- java-interview-flashcards/src/App.jsx
+++ java-interview-flashcards/src/App.jsx
@@ -3,20 +3,27 @@ import './App.css'
 import Flashcard from './components/Flashcard'
 
 function App() {
-
-  const [currentIndex, setNextIndex] = useState(0);
-
   const javaQuestions = [
-    "What is the difference between an interface and an abstract class in Java?",
-    "What is the difference between a static and a non-static method in Java?",
-    "What is the difference between an ArrayList and a LinkedList in Java?",
-    "What is the purpose of the 'final' keyword in Java?",
-    "What is the 'this' keyword in Java and how is it used?",
-    "What is the difference between a checked and an unchecked exception in Java?",
-    "What is the purpose of the 'synchronized' keyword in Java?",
-    "What is the difference between '==' and '.equals()' in Java?",
-    "What is the difference between a HashMap and a TreeMap in Java?",
-    "What is the purpose of the 'transient' keyword in Java?"
+    "1) What is the difference between an interface and an abstract class in Java?",
+    "2) What is the difference between a static and a non-static method in Java?",
+    "3) What is the difference between an ArrayList and a LinkedList in Java?",
+    "4) What is the purpose of the 'final' keyword in Java?",
+    "5) What is the 'this' keyword in Java and how is it used?",
+    "6) What is the difference between a checked and an unchecked exception in Java?",
+    "7) What is the purpose of the 'synchronized' keyword in Java?",
+    "8) What is the difference between '==' and '.equals()' in Java?",
+    "9) What is the difference between a HashMap and a TreeMap in Java?",
+    "10) What is the purpose of the 'transient' keyword in Java?",
+    "11) One/two word answer: What is Java?",
+    "12) One/two word answer: What is a class?",
+    "13) One/two word answer: What is an object?",
+    "14) One/two word answer: What is inheritance?",
+    "15) One/two word answer: What is polymorphism?",
+    "16) One/two word answer: What is an interface?",
+    "17) One/two word answer: What is encapsulation?",
+    "18) One/two word answer: What is a constructor?",
+    "19) One/two word answer: What is a method?",
+    "20) One/two word answer: What is a package?"
   ];
 
   const javaAnswers = [
@@ -29,30 +36,172 @@ function App() {
     "The 'synchronized' keyword is used to create a synchronized block of code, which ensures that only one thread can access that block of code at a time. It is used to prevent race conditions in multi-threaded programs.",
     "'==' is used to compare the reference of two objects, while '.equals()' is used to compare the values of two objects. The '.equals()' method is often overridden by classes to provide custom equality checking.",
     "A HashMap is a hash table that allows null values and does not maintain any order, while a TreeMap is a sorted map that does not allow null keys and maintains its keys in a sorted order.",
-    "The 'transient' keyword is used to indicate that a variable should not be serialized when an object is serialized. It is used to exclude certain variables from the serialization process."
+    "The 'transient' keyword is used to indicate that a variable should not be serialized when an object is serialized. It is used to exclude certain variables from the serialization process.",
+    "Programming Language",
+    "Blueprint",
+    "Instance",
+    "Reusability",
+    "Flexibility",
+    "Contract",
+    "Data-hiding",
+    "Initialization",
+    "Function",
+    "Namespace"
   ];
 
+  const [currentIndex, setIndex] = useState(0);
+  const [questions, setQuestions] = useState(javaQuestions);
+  const [answers, setAnwers] = useState(javaAnswers);
+  const [shuffleOn, setShuffleOn] = useState(false);
+  const [isNext, setIsNext] = useState(true);
+  const [isPrevious, setIsPrevious] = useState(false);
+  const [isSmallAnswer, setIsSmallAnswer] = useState(false);
+  const [userAnswer, setUserAnswer] = useState("Type your answer here");
+  const [buttonColor, setButtonColor] = useState("blueviolet");
+  const [currentStreak, setCurrentStreak] = useState(0);
+  const [longestStreak, setLongestStreak] = useState(0);
+
   const getNextQuestion = () => {
-    const randomIndex = Math.floor(Math.random() * 10);
-    setNextIndex(randomIndex);
+    if (currentIndex != questions.length - 1 && questions[currentIndex + 1].includes("One/two word answer")) {
+      setIsSmallAnswer(true);
+    } else {
+      setIsSmallAnswer(false);
+    }
+
+    if (currentIndex != answers.length - 1) {
+      setIndex(currentIndex + 1);
+    }
+    
+    if (currentIndex == answers.length - 2) {
+      setIsNext(false);
+    }
+
+    setIsPrevious(true);
+    setUserAnswer("");
+    setButtonColor("blueviolet");
   }
 
+  const getPreviousQuestion = () => {
+    if (currentIndex != 0 && questions[currentIndex - 1].includes("One/two word answer")) {
+      setIsSmallAnswer(true);
+    } else {
+      setIsSmallAnswer(false);
+    }
+
+    if (currentIndex != 0) {
+      setIndex(currentIndex - 1); 
+    }
+
+    if (currentIndex == 1) {
+      setIsPrevious(false);
+    }
+
+    setIsNext(true);
+    setUserAnswer("");
+    setButtonColor("blueviolet");
+  }
+
+  const doShuffle = () => {
+    const shuffledIndices = [...Array(javaQuestions.length).keys()]
+    
+    // Fisher - Yates shuffling algorithm
+    for (let i = shuffledIndices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledIndices[i], shuffledIndices[j]] = [shuffledIndices[j], shuffledIndices[i]];
+    }
+
+    const shuffledQuestions = shuffledIndices.map((index) => javaQuestions[index]);
+    const shuffledAnswers = shuffledIndices.map((index) => javaAnswers[index]);
+
+    var currentQuestion;
+
+    if (shuffleOn) {
+      setShuffleOn(false);
+      setQuestions(javaQuestions);
+      setAnwers(javaAnswers);
+      currentQuestion = javaQuestions[0];
+    } else {
+      setShuffleOn(true);
+      setQuestions(shuffledQuestions);
+      setAnwers(shuffledAnswers);
+      currentQuestion = shuffledQuestions[0];
+    }
+
+    setIndex(0);
+    setIsNext(true);
+    setIsPrevious(false);
+    setIsSmallAnswer(false);
+    setUserAnswer("");
+    setButtonColor("blueviolet");
+
+    if (currentQuestion.includes("One/two word answer")) {
+      setIsSmallAnswer(true);
+    }
+  }
+
+  const handleTextClick = () => {
+    if (userAnswer == "Type your answer here") {
+      setUserAnswer("");
+    }
+  }
+
+  const handleChange = (event) => {
+    setUserAnswer(event.target.value)
+  }
+
+  const handleSubmit = () => {
+    if (userAnswer.toLowerCase() == answers[currentIndex].toLowerCase()) {
+      setButtonColor('green');
+      setCurrentStreak(currentStreak + 1);
+    } else {
+      setButtonColor('red');
+      if (currentStreak > longestStreak) {
+        setLongestStreak(currentStreak);
+      }
+      setCurrentStreak(0);
+    }
+  }
+  
   return (
     <div className="App">
+      
       <div class="website-title">
         <h1> Java Interview Flashcards </h1>
-        <h3> Refine your skills for the upcoming internship interview for Java </h3>
-        <h4> Total Number of Cards: {javaQuestions.length} </h4>
+        <h2> Refine your skills for the upcoming internship interview for Java </h2>
+        <h3> Total Number of Cards: {questions.length} </h3>
+        <h3> Longest Streak: {longestStreak} &nbsp; &nbsp; &nbsp; Current Streak: {currentStreak}</h3>
       </div>
+      
       <div className='flashcard-container'>
         <Flashcard 
-          question={javaQuestions[currentIndex]}
-          answer={javaAnswers[currentIndex]}
+          question={questions[currentIndex]}
+          answer={answers[currentIndex]}
         />
       </div>
-      <button className="next-button" onClick={getNextQuestion}>
-        Next ⏭️
-      </button>
+
+      <div className="input-container" style={{visibility: isSmallAnswer? 'visible' : 'hidden'}}>
+        <h2>Guess the answer</h2>
+        
+        <input type="text" value={userAnswer} onClick={handleTextClick} onChange={handleChange}/>
+        
+        <button className='submit-button' onClick={handleSubmit} style={{backgroundColor: buttonColor}}>
+          Submit
+        </button>
+      </div>
+      
+      <div className='buttons-container'>
+        <button className='previous-button' onClick={getPreviousQuestion} style={{visibility: isPrevious? 'visible' : 'hidden'}}>
+          👈 Previous
+        </button>
+        
+        <button className="next-button" onClick={getNextQuestion} style={{visibility: isNext? 'visible' : 'hidden'}}>
+          Next 👉 
+        </button>
+        
+        <button className='shuffle-cards-button' onClick={doShuffle} style={{backgroundColor: shuffleOn? 'burlywood' : 'blueviolet'}}>
+          Shuffle Cards
+        </button>
+      </div>
     </div>
   )
 }
